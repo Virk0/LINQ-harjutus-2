f85ee9771ea65b1c7fd8249c3da9c08a0841f344
Firesword12 <karl.orul@mail.ee>
2018-02-22 09:22:25 +0200

    ok

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LINQ2
 {
@@ -10,6 +8,29 @@ namespace LINQ2
     {
         static void Main(string[] args)
         {
+            /*
+            Kasutades võimalikult palju LINQ võimalusi:
+            1.Lugege kokku mitu sõna on tekstis.
+            2.Mitu erinevat tähte on tekstis.
+            3.Lugeda kokku mitu korda esineb erinevaid tähti tekstis ning kirjutada tulemused kasutajale välja järjestatult.
+            4.Kirjutada kasutajale välja kõik sõnad nii, et ühtegi sõna poleks rohkem üks kord.
+            */
+            #region Harjutus 1
+            string text = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "text");
+            System.Console.ReadLine();
+            using (StreamReader newText = new StreamReader(text + @"\tekst.txt"))
+            {
+                string count = newText.ReadToEnd();
+                string[] ssize = count.Split(new char[] { ' ' });
+                System.Console.WriteLine("There are {0} words in this file", ssize.Count());
+                System.Console.ReadLine();
+            }
+            #endregion
+
+            #region Harjutus 2
+            
+            #endregion
+
         }
     }
 }
